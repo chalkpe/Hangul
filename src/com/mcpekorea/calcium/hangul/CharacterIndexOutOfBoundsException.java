171c89543842fd552edcd213c022bc911379ea4b
Chalk <amato17@naver.com>
2014-10-31 00:10:09 +0900

    Modified in a better way

---
+++ src/com/mcpekorea/calcium/hangul/CharacterIndexOutOfBoundsException.java
@@ -0,0 +1,9 @@
+package com.mcpekorea.calcium.hangul;
+
+public class CharacterIndexOutOfBoundsException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public CharacterIndexOutOfBoundsException(final char wrongChar){
+		super(String.format("Illegal character %c - must be 0xAC00 <= x <= 0xD7AF", wrongChar));
+	}
+}
--- src/com/mcpekorea/calcium/hangul/Hangul.java
+++ src/com/mcpekorea/calcium/hangul/Hangul.java
@@ -1,46 +1,44 @@
 package com.mcpekorea.calcium.hangul;
 
 public class Hangul {
-	public static final int MODE_OBJECT = 0; // 을/를
-	public static final int MODE_NOUN = 1; // 은/는
-	public static final int MODE_CONJUNCTION = 2; // 이/가
-	
-	public static final char CHAR_EUL = 51012;//을
-	public static final char CHAR_LEUL = 47484;//를
-	public static final char CHAR_I = 51060;//이
-	public static final char CHAR_GA = 44032;//가
-	public static final char CHAR_EUN = 51008;//은
-	public static final char CHAR_NEUN = 45716;//는
-	
-	public static boolean hasConsonant(final char unicode){
-		
-		return (unicode-44032)%28 != 0;
+	public static enum Mode {
+		OBJECT, NOUN, CONJUNCTION;
 	}
 	
-	public static char getNext(final char unicode, int mode){
-		if(unicode < 0xAC00 || unicode > 0xD7AF){
-			return 0;
-		}
+	public static final char CHAR_BEGIN = 0xAC00; //-> 가
+	public static final char CHAR_END = 0xD7A3;   //-> 힣
+	
+	public static final char CHAR_EUL = 51012;    //-> 을
+	public static final char CHAR_LEUL = 47484;   //-> 를
+	public static final char CHAR_I = 51060;      //-> 이
+	public static final char CHAR_GA = 44032;     //-> 가
+	public static final char CHAR_EUN = 51008;    //-> 은
+	public static final char CHAR_NEUN = 45716;   //-> 는
+	
+	
+	public static boolean hasFinalConsonant(final char unicode){
+		if(unicode < CHAR_BEGIN || unicode > CHAR_END)
+			throw new CharacterIndexOutOfBoundsException(unicode);
+		
+		return (unicode - CHAR_BEGIN) % 28 != 0;
+	}
+	
+	public static char getNext(final char unicode, final Hangul.Mode mode){
+		if(unicode < CHAR_BEGIN || unicode > CHAR_END)
+			throw new CharacterIndexOutOfBoundsException(unicode);
+		
 		switch(mode){
-		case MODE_OBJECT:
-			if(hasConsonant(unicode)){
-				return CHAR_EUL;
-			}else{
-				return CHAR_LEUL;
-			}
-		case MODE_NOUN:
-			if(hasConsonant(unicode)){
-				return CHAR_EUN;
-			}else{
-				return CHAR_NEUN;
-			}
-		case MODE_CONJUNCTION:
-			if(hasConsonant(unicode)){
-				return CHAR_I;
-			}else{
-				return CHAR_GA;
-			}
+			case OBJECT:
+				return hasFinalConsonant(unicode) ? CHAR_EUL : CHAR_LEUL;
+					
+			case NOUN:
+				return hasFinalConsonant(unicode) ? CHAR_EUN : CHAR_NEUN;
+				
+			case CONJUNCTION:
+				return hasFinalConsonant(unicode) ? CHAR_I : CHAR_GA;
+				
+			default:
+				throw new IllegalArgumentException();
 		}
-		return 0;
 	}
 }
