b83b00f26800ccd8b40e5ae99e5428b9d0e827c0
Chalk <amato0617@gmail.com>
2014-10-31 23:00:01 +0900

    Added "-야" and changed method name

--- src/com/mcpekorea/calcium/hangul/Hangul.java
+++ src/com/mcpekorea/calcium/hangul/Hangul.java
@@ -5,7 +5,8 @@ public class Hangul {
 		OBJECT,      //-> 을/를
 		NOUN,        //-> 은/는
 		CONJUNCTION, //-> 이/가
-		AND;         //-> 와/과
+		AND,         //-> 과/와
+		PRONOUN;     //-> 아/야
 	}
 	
 	public static final char CHAR_BEGIN = 0xAC00;  //-> 가
@@ -13,12 +14,14 @@ public class Hangul {
 	
 	public static final char CHAR_EUL = 0xC744;    //-> 을
 	public static final char CHAR_LEUL = 0xB97C;   //-> 를
-	public static final char CHAR_I = 0xC774;      //-> 이
-	public static final char CHAR_GA = CHAR_BEGIN; //-> 가
 	public static final char CHAR_EUN = 0xC740;    //-> 은
 	public static final char CHAR_NEUN = 0xB294;   //-> 는
-	public static final char CHAR_WA = 0xC640;     //-> 와
+	public static final char CHAR_I = 0xC774;      //-> 이
+	public static final char CHAR_GA = CHAR_BEGIN; //-> 가
 	public static final char CHAR_GWA = 0xACFC;    //-> 과
+	public static final char CHAR_WA = 0xC640;     //-> 와
+	public static final char CHAR_A = 0xC544;      //-> 아
+	public static final char CHAR_YA = 0xC57C;     //-> 야
 	
 	
 	public static boolean hasFinalConsonant(final char unicode){
@@ -28,7 +31,7 @@ public class Hangul {
 		return (unicode - CHAR_BEGIN) % 28 != 0;
 	}
 	
-	public static char getNext(final char unicode, final Hangul.Mode mode){
+	public static char getPostposition(final char unicode, final Hangul.Mode mode){
 		if(unicode < CHAR_BEGIN || unicode > CHAR_END)
 			throw new CharacterIndexOutOfBoundsException(unicode);
 		
@@ -42,10 +45,13 @@ public class Hangul {
 			case CONJUNCTION:
 				return hasFinalConsonant(unicode) ? CHAR_I : CHAR_GA;
 			
-            case AND:
+			case AND:
 				return hasFinalConsonant(unicode) ? CHAR_GWA : CHAR_WA;
 			
-            default:
+			case PRONOUN:
+				return hasFinalConsonant(unicode) ? CHAR_A : CHAR_YA;
+				
+			default:
 				throw new IllegalArgumentException();
 		}
 	}
