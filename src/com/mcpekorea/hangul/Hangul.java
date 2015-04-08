a9cc02043b47f6077a7f3126f4e5f53cadba7338
ChalkPE <amato0617@gmail.com>
2015-04-09 08:50:44 +0900

    Added an initial consonants' array (ㄱ, ㄲ, ㄴ, ㄷ, ㄸ, ㄹ, ㅁ, ㅂ, ㅃ, ㅅ, ㅆ, ㅇ, ㅈ, ㅉ, ㅊ, ㅋ, ㅌ, ㅍ, ㅎ)

--- src/com/mcpekorea/hangul/Hangul.java
+++ src/com/mcpekorea/hangul/Hangul.java
@@ -23,6 +23,11 @@ public class Hangul {
 	public static final char CHAR_GWA  = 0xACFC; //-> 과
 	public static final char CHAR_WA   = 0xC640; //-> 와
 	
+	public static final char[] INITIAL_CONSONANTS = new char[]{
+	    0x3131, 0x3132, 0x3134, 0x3137, 0x3138, 0x3139, 0x3141, 0x3142, 0x3143, 0x3145, 0x3146, 0x3147, 0x3148, 0x3149, 0x314a, 0x314b, 0x314c, 0x314d, 0x314e
+	    //ㄱ, ㄲ, ㄴ, ㄷ, ㄸ, ㄹ, ㅁ, ㅂ, ㅃ, ㅅ, ㅆ, ㅇ, ㅈ, ㅉ, ㅊ, ㅋ, ㅌ, ㅍ, ㅎ
+	};
+	
 	public static final Pattern pattern = Pattern.compile("(\\S)\\[\\[(이|가|을|를|과|와|은|는|NOMINATIVE|ACCUSATIVE|COMITATIVE|TOPIC)\\]\\]");
 	
 	public static Hangul.Type getType(String typeText){
